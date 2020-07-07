fa22131948d516e008e6fa3f42bd75998e7fe9c0
Callum Michael Waters <cmwaters19@gmail.com>
2020-07-07 17:08:57 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+
+module.exports = {
+    mode: 'development',
+    watch: false,
+    entry: {
+        game: './game/game.ts'
+    },
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/dist',
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+};
