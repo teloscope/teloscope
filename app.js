0b719d386031bebd1e1b7e5e89090c53eaa76065
Callum <cmwaters19@gmail.com>
2020-07-26 18:04:52 +0200

    connect database

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var Cookies = require('cookies')
+var Keygrip = require("keygrip")
+// var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost/teloscope", { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log("Connected to database"));
 
 var indexRouter = require('./routes/index');
 var devRouter = require('./routes/dev');
@@ -16,7 +25,9 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+let keys = Keygrip(["SEKRIT2", "SEKRIT1"], 'sha256', 'hex')
+app.use(Cookies.express(keys))
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
--- routes/dev.js
+++ routes/dev.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const TestModel = require('../models/test');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,41 +8,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/intro', function(req, res, next) {
+  console.log("writing to database")
+  const testEntry = new TestModel({
+    name: "Jimmy"
+  })
+  testEntry.save()
   res.render('introduction.pug');
 });
 
 // add game routes
-router.get('/alpha', function(req, res, next) {
+router.get('/alpha', async function(req, res, next) {
+  const test = await TestModel.find()
+  console.log(test)
   res.redirect('/dev/alpha/instructions')
 });
 
 router.get('/alpha/instructions', function(req, res, next) {
-  res.render('alpha/instructions.pug');
+  res.render('alpha/instructions.pug', {game: "alpha"});
 });
 
 router.get('/alpha/game', function(req, res, next) {
-  res.render('alpha/game.pug');
+  res.render('alpha/game.pug',  {game: "alpha"});
 });
 
 router.get('/alpha/review', function(req, res, next) {
-  res.render('alpha/end.pug');
+  res.render('alpha/end.pug',  {game: "alpha"});
 });
-
 
 router.get('/beta', function(req, res, next) {
   res.redirect('/dev/beta/instructions')
 });
 
 router.get('/beta/instructions', function(req, res, next) {
-  res.render('beta/instructions.pug');
+  res.render('beta/instructions.pug', {game: "beta"});
 });
 
 router.get('/beta/game', function(req, res, next) {
-  res.render('beta/game.pug');
+  res.render('beta/game.pug', {game: "beta"});
 });
 
 router.get('/beta/review', function(req, res, next) {
-  res.render('beta/end.pug');
+  res.render('beta/end.pug', {game: "beta"});
 });
 
 
@@ -50,15 +57,15 @@ router.get('/delta', function(req, res, next) {
 });
 
 router.get('/delta/instructions', function(req, res, next) {
-  res.render('delta/instructions.pug');
+  res.render('delta/instructions.pug', {game: "delta"});
 });
 
 router.get('/delta/game', function(req, res, next) {
-  res.render('delta/game.pug');
+  res.render('delta/game.pug', {game: "delta"});
 });
 
 router.get('/delta/review', function(req, res, next) {
-  res.render('delta/end.pug');
+  res.render('delta/end.pug', {game: "delta"});
 });
 
 module.exports = router;
